3b859865cbc91da7045a10e63253b0cff98014d9
fikinur10 <fikinur@gmail.com>
2023-07-06 14:23:27 +0700

    Initial Commit

---
+++ wisata/wisataApp.swift
@@ -0,0 +1,17 @@
+//
+//  wisataApp.swift
+//  wisata
+//
+//  Created by Fiki Nur on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct wisataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
